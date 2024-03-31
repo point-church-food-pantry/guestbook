4dff6ed9e15c4efaa904132b54966d059dcd2713
point-church-food-pantry <154100866+point-church-food-pantry@users.noreply.github.com>
2024-03-30 23:53:06 -0400

    Add files via upload

--- guestbook/settings.py
+++ guestbook/settings.py
@@ -122,3 +122,13 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Django-jsignature global options
+JSIGNATURE_WIDTH = 500 * 2
+JSIGNATURE_HEIGHT = 200
+
+
+
+
+
+
